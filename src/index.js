5dc20ca6ef90538fcf9b665ae99a0ed894498713
Julio <jrab997@gmail.com>
2022-04-04 08:22:24 -0500

    Se agregaron funciones para el manejo del array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 const d = document;
-d.addEventListener('DOMContentLoaded', (e) => {});
+const $arr = d.getElementById('array');
+d.addEventListener('DOMContentLoaded', (e) => {
+  setIndex();
+});
 
-function atMethod(index) {}
+function handlerButtons() {
+  d.addEventListener('click', (e) => {
+    const arr = eval(getArrayValues());
+    if (e.target.matches('#at')) {
+      let index = prompt('Insert value');
+      let arrValue = arr.at(index);
+    }
+  });
+}
+
+const getArrayValues = () => {
+  $arr.textContent.substring(10);
+};
+
+const atMethod = (arr) => {};
+
+function setIndex() {
+  d.querySelectorAll('#array span').forEach(($span, index) => {
+    $span.id = index;
+  });
+}
