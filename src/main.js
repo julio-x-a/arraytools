7505dbe431cfcf2dc7a83da947daf98f9f4e9eec
Julio <jrab997@gmail.com>
2022-05-04 17:12:37 -0500

    minor changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import './style.css';
+import './styles/style.css';
 import ANI from './animations.js';
 
 const d = document;
-const $arr = d.getElementById('array');
+
+/**
+ * It returns an array of all the span elements in the array page.
+ */
+const arraypage = () => Array.from(d.querySelectorAll('#array span'));
 
 d.addEventListener('DOMContentLoaded', (e) => {
   setIndex();
@@ -16,15 +20,21 @@ function handlerButtons() {
   });
 }
 
+/**
+ * It takes the index of the element you want to animate, finds the element with that index,
+ * and then animates it.
+ */
 const atMethod = () => {
   let index = window.prompt('Type index');
-  const $spans = Array.from(d.querySelectorAll('#array span'));
-  const element = $spans.find(($span) => $span.id === index);
+  const element = arraypage().find(($span) => $span.id === index);
   element?.animate(ANI.rollInBlurredTop, { duration: 1000, iterations: 1 });
 };
 
+/**
+ * It removes the last element of the array, and then removes the comma that follows it.
+ */
 const popMethod = async () => {
-  const element = Array.from(d.querySelectorAll('#array span')).at(-1);
+  const element = arraypage().at(-1);
   if (element) {
     try {
       let state = await element?.animate(ANI.wobbleHorBottom, {
@@ -44,10 +54,12 @@ const popMethod = async () => {
 
 const pushMethod = () => {
   const newElement = prompt('Insert value:');
-  const $spans = Array.from(d.querySelectorAll('#array span'));
-  $spans.at(-1).insertAdjacentText('afterend', ', ');
+  arraypage.at(-1).insertAdjacentText('afterend', ', ');
 };
 
+/**
+ * It loops through all the spans in the array and sets their id to their index.
+ */
 function setIndex() {
   d.querySelectorAll('#array span').forEach(($span, index) => {
     $span.id = index;
